a462181dd2fcd67e27417c15f523a4f3b80dc3e1
Eric Yuan <89362858+EricAtomic@users.noreply.github.com>
2024-03-15 21:42:33 +1300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "AtomicSwiftUISDK",
+    products: [
+        .library(
+            name: "AtomicSwiftUISDK",
+            targets: ["AtomicSwiftUISDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/EricAtomic/releaseTest.git", from: "1.0.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AtomicSwiftUISDK",
+            path: "iOS/AtomicSwiftUISDK.xcframework"),
+        .target(
+            name: "AtomicSwiftUISDK",
+            dependencies: ["AtomicSDK"])
+    ]
+)
