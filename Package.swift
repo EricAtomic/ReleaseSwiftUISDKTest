0b664f9043b176f5e0b077afd4a7d68d1c505bc0
Eric Yuan <89362858+EricAtomic@users.noreply.github.com>
2024-03-15 21:49:48 +1300

    35.0.0

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
             targets: ["AtomicSwiftUISDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/EricAtomic/releaseTest.git", from: "1.0.0")
+        .package(url: "https://github.com/EricAtomic/releaseTest.git", from: "35.0.0")
     ],
     targets: [
         .binaryTarget(
